16cb96992e43bd23869ef83b2a25af0d5f5513ec
Janak Malla <jmalla588@gmail.com>
2020-04-28 16:57:39 -0500

    Updates

---
+++ precache-manifest.350e4dd6599a32a20efba2a912664220.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b83350a51d8973825cd406f22ece2f",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "2bd1d49ebd4cb5453d17",
+    "url": "/react-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "45c4c0f1c7c2f005628f",
+    "url": "/react-test/static/js/2.4e0b116d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.4e0b116d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bd1d49ebd4cb5453d17",
+    "url": "/react-test/static/js/main.fea2bb5f.chunk.js"
+  },
+  {
+    "revision": "73715cc933037ceccd4f",
+    "url": "/react-test/static/js/runtime-main.d2ebb1ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
